6c8eb72921597b1de4b328a3b0ac95f23effdc4b
@anulman <anulman@gmail.com>
2018-04-04 23:50:46 -0400

    refactor(index): use new headTags pattern

---
+++ app/pods/index/head-tags.js
@@ -0,0 +1,8 @@
+export default [{
+  type: 'meta',
+  tagId: 'meta-pinterest-verify',
+  attrs: {
+    property: 'p:domain_verify',
+    content: 'a8d507e48a3dd3d7e3a2b68b9a79937c'
+  }
+}]
--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,15 +1,6 @@
 import Route from '@ember/routing/route';
-import { computed } from '@ember/object';
+import headTags from './head-tags';
 
 export default Route.extend({
-  headTags: computed(function() {
-    return [{
-      type: 'meta',
-      tagId: 'meta-pinterest-verify',
-      attrs: {
-        property: 'p:domain_verify',
-        content: 'a8d507e48a3dd3d7e3a2b68b9a79937c'
-      }
-    }]
-  })
+  headTags
 });
