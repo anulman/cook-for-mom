5b292b76c22e0258b7705f12725c9429da77447b
@anulman <anulman@gmail.com>
2018-04-10 02:33:03 -0400

    refactor(lessons): rm deprecated lessons routing

--- app/pods/lessons/knife-skills/controller.js
+++
@@ -1,9 +0,0 @@
-import Controller, { inject as controller } from '@ember/controller';
-
-import sections from './sections';
-
-export default Controller.extend({
-  lessons: controller(),
-
-  sections
-});
--- app/pods/lessons/knife-skills/route.js
+++
@@ -1,6 +0,0 @@
-import Route from '@ember/routing/route';
-import headTags from './head-tags';
-
-export default Route.extend({
-  headTags
-});
