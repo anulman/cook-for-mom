519fde1f3f89bf33ba9c2771a12589151167c68b
@anulman <anulman@gmail.com>
2018-04-18 03:10:38 -0400

    refactor(section-info/title): permit passing title as positional param

--- app/pods/components/section-info/title/component.js
+++ app/pods/components/section-info/title/component.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
 
-export default Component.extend({
-  tagName: 'h2'
+const TitleComponent = Component.extend({
+  tagName: 'h2',
+  title: null
 });
+
+TitleComponent.reopenClass({
+  positionalParams: ['title']
+});
+
+export default TitleComponent;
