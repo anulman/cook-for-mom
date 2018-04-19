87422da2d3a570f9c47af1db33d83817ab1ff0d3
@anulman <anulman@gmail.com>
2018-04-19 05:08:30 -0400

    refactor(nav-bar): start rm'ing items.link-to pattern

--- app/pods/components/nav-bar/items/component.js
+++ app/pods/components/nav-bar/items/component.js
@@ -3,6 +3,5 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['nav-wrapper'],
 
-  itemComponent: 'nav-bar/items/item',
-  linkToComponent: 'nav-bar/items/link-to'
+  itemComponent: 'nav-bar/items/item'
 });
