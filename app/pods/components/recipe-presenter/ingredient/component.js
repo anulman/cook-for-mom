0856dfe5cfcb27197fad8e79a81a9ad479f0cd71
@anulman <anulman@gmail.com>
2018-04-21 18:52:07 -0400

    feat(ingredient): add click & strikeout logic

--- app/pods/components/recipe-presenter/ingredient/component.js
+++ app/pods/components/recipe-presenter/ingredient/component.js
@@ -1,11 +1,28 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 const IngredientComponent = Component.extend({
   tagName: 'li',
+  classNameBindings: ['isStruckOut:is-struck-out'],
+
+  metrics: service(),
 
   ingredient: null,
+  isStruckOut: false,
 
-  notesComponent: 'recipe-presenter/notes'
+  notesComponent: 'recipe-presenter/notes',
+
+  click() {
+    let ingredient = this.get('ingredient.ingredient');
+    let recipe = this.get('ingredient.recipe.id');
+
+    this.toggleProperty('isStruckOut');
+    this.get('metrics').trackEvent('Segment', {
+      event: 'toggled ingredient',
+      ingredient,
+      recipe
+    });
+  }
 });
 
 IngredientComponent.reopenClass({
