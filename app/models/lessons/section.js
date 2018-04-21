26c9dbd917ed47c327682ead17efe9a048da7abf
@anulman <anulman@gmail.com>
2018-04-21 18:28:23 -0400

    feat(lesson/signup-form): use signup form in lessons/lesson template when section is locked

--- app/models/lessons/section.js
+++ app/models/lessons/section.js
@@ -5,6 +5,7 @@ const { Model, attr, belongsTo, hasMany } = DS;
 export default Model.extend({
   title: attr('string'),
   slug: attr('string'),
+  isLocked: attr('boolean'),
   description: attr('string'),
   imageUrl: attr('string'),
   videoSources: attr(), // an object with keys { src, type }
