5f0b97d3a4eeb1cfab0c13eb8cab8cb69a008966
@anulman <anulman@gmail.com>
2018-04-21 22:07:45 -0400

    refactor(lessons): reroute index to slow-cooking

--- app/pods/lessons/index/route.js
+++ app/pods/lessons/index/route.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   redirect() {
-    this.replaceWith('lessons.lesson', 'knife-skills');
+    this.replaceWith('lessons.lesson', 'slow-cooking');
   }
 });
